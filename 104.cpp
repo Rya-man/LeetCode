70db2243dddd2602ee74ad7cc329df5d534a8f5f
Aryaman Prakash <arpr234@gmail.com>
2024-02-20 20:02:06 +0530

    104. Maximum Depth of Binary Tree

--- 104.cpp
+++ 104.cpp
@@ -24,17 +24,15 @@ The number of nodes in the tree is in the range [0, 104].
 using namespace std;
 
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-
+ * Definition for a binary tree node.*/
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
 class Solution {
  public:
   int maxDepth(TreeNode* root) {
@@ -44,5 +42,6 @@ class Solution {
     if (root == nullptr)
       return 0;
     return 1 + max(maxDepth(root->left), maxDepth(root->right));
+    
   }
 };
