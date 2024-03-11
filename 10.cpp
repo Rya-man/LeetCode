fd75f2ace8730200c4566a07a17194e6c4472ffd
Aryaman Prakash <arpr234@gmail.com>
2024-03-11 23:39:31 +0530

    10. Regular Expression Matching

---
+++ 10.cpp
@@ -0,0 +1,72 @@
+/*Given an input string s and a pattern p, implement regular expression matching with support for '.' and '*' where:
+
+'.' Matches any single character.​​​​
+'*' Matches zero or more of the preceding element.
+The matching should cover the entire input string (not partial).
+
+ 
+
+Example 1:
+
+Input: s = "aa", p = "a"
+Output: false
+Explanation: "a" does not match the entire string "aa".
+Example 2:
+
+Input: s = "aa", p = "a*"
+Output: true
+Explanation: '*' means zero or more of the preceding element, 'a'. Therefore, by repeating 'a' once, it becomes "aa".
+Example 3:
+
+Input: s = "ab", p = ".*"
+Output: true
+Explanation: ".*" means "zero or more (*) of any character (.)".
+ 
+
+Constraints:
+
+1 <= s.length <= 20
+1 <= p.length <= 20
+s contains only lowercase English letters.
+p contains only lowercase English letters, '.', and '*'.
+It is guaranteed for each appearance of the character '*', there will be a previous valid character to match.*/
+
+
+#include<bits/stdc++.h>
+
+
+using namespace std;
+class Solution {
+ public:
+  bool isMatch(string s, string p) {
+      ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    const int m = s.length();
+    const int n = p.length();
+    // dp[i][j] := true if s[0..i) matches p[0..j)
+    vector<vector<bool>> dp(m + 1, vector<bool>(n + 1));
+    dp[0][0] = true;
+
+    auto isMatch = [&](int i, int j) -> bool {
+      return j >= 0 && p[j] == '.' || s[i] == p[j];
+    };
+
+    for (int j = 0; j < p.length(); ++j)
+      if (p[j] == '*' && dp[0][j - 1])
+        dp[0][j + 1] = true;
+
+    for (int i = 0; i < m; ++i)
+      for (int j = 0; j < n; ++j)
+        if (p[j] == '*') {
+          // The minimum index of '*' is 1.
+          const bool noRepeat = dp[i + 1][j - 1];
+          const bool doRepeat = isMatch(i, j - 1) && dp[i][j + 1];
+          dp[i + 1][j + 1] = noRepeat || doRepeat;
+        } else if (isMatch(i, j)) {
+          dp[i + 1][j + 1] = dp[i][j];
+        }
+
+    return dp[m][n];
+  }
+};
